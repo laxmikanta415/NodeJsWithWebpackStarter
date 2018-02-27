0bb1c8961f794321579516e8f3fa335b09c6ddab
Laxmikanta Nayak <laxmikantanayak415@gmail.com>
2018-02-27 21:30:32 +0530

    added context

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,12 @@ const NodeExternals = require("webpack-node-externals");
 const StartServerPlugin = require("start-server-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  context: path.resolve(__dirname, "src"),
+  entry: "./index.js",
+  node: {
+    __dirname: true,
+    __filename: true
+  },
   watch: true,
   output: {
     filename: "bundle.server.js",
